c43ff3d20855f59c18f8496cff5e51464857c189
Czarek Polak <czarek.pmp@gmail.com>
2021-07-26 21:32:11 +0200

    Add support for cutting files by provided IDs. The third argument is tne name of the file saved in the /temp dir

--- analyze_wav/take_frame_by_number.py
+++ analyze_wav/take_frame_by_number.py
@@ -6,8 +6,8 @@ import argparse
 parser = argparse.ArgumentParser(description = "Cut out desired frame by id and save it to temp directory with the id number.")
 parser.add_argument('-i', '--index', help='provide id of frames you want to save. use comma as delimiter. Indexing start with zero.', type=str)
 parser.add_argument('-l', '--length', type=int, metavar='', help='length of frame in secs. Default value 0.2 s')
+parser.add_argument('-m', '--my', type=str, metavar='', help='Cut your fragment of signal by providing first and last frame id. The third argument is the name of file. Args seperated by coma')
 args = parser.parse_args()
-frame_ids = [int(item) for item in args.index.split(',')]
 
 d = dirname(dirname(abspath(__file__)))
 sys.path.append(d)
@@ -29,10 +29,30 @@ if __name__ == '__main__':
 
     SR, data = wavfile.read(INPUT_PATH)
 
+    print("Do przetworzenia jest {} ramek".format(int(len(data) / (FRAME_SIZE * SR))))
+
     # SET number of samples in one frame
     SAMPLES_PER_FRAME = int(FRAME_SIZE * SR)
-            
-    for id in frame_ids:
-        frame = data[int(id*SAMPLES_PER_FRAME) : int((id + 1)*SAMPLES_PER_FRAME)]
-        wavfile.write(join(OUTPUT_DIR, str(id)) + '.wav', SR, frame)
+    
+    if args.index is not None:
+        frame_ids = [int(item) for item in args.index.split(',')]
+        for id in frame_ids:
+            frame = data[int(id*SAMPLES_PER_FRAME) : int((id + 1)*SAMPLES_PER_FRAME)]
+            wavfile.write(join(OUTPUT_DIR, str(id)) + '.wav', SR, frame)
 
+        print("Przetworzono {} ramek o długości {} s każda".format(len(frame_ids), FRAME_SIZE))
+
+    if args.my is not None:
+        
+        arguments = [item for item in args.my.split(',')]
+        name = str(arguments[-1])
+        start = int(arguments[0])
+        stop = int(arguments[1])
+        frame = data[start : int(stop + 1)]
+
+        frame = data[int(start * SAMPLES_PER_FRAME) : int((stop + 1) * SAMPLES_PER_FRAME)]
+        
+        wavfile.write(join(OUTPUT_DIR, name + '.wav'), SR, frame)
+        print("Zapisano  {} ".format(join(OUTPUT_DIR, name + '.wav')))
+
+    print('----------------------- END --------------------------------')
