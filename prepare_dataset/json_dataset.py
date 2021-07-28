3cac1b42e68d27daf9218c97789bf5ad048be22f
Czarek Polak <czarek.pmp@gmail.com>
2021-07-28 14:42:48 +0200

    Prepare smaller frames to save

--- prepare_dataset/json_dataset.py
+++ prepare_dataset/json_dataset.py
@@ -1,20 +1,24 @@
 from os.path import dirname, abspath, join
 import sys
+from numpy.core import records
 import pandas as pd
+from pandas.core import frame
 from scipy.io import wavfile
 import json
+import numpy as np
 
 DIR_PATH = dirname(dirname(abspath(__file__)))
 sys.path.append(DIR_PATH)
 
 DATA_PATH = "csv\Frame_descr.csv"
 PATH = join(DIR_PATH,  DATA_PATH)
-WAV_PATH_REL = "wav\long\data_long.wav"
+WAV_PATH_REL = "wav\long\data_long_mono.wav"
 WAV_PATH= join(DIR_PATH,  WAV_PATH_REL)
 
 # Frame size in sec
 FRAME_SIZE = 0.2
 TARGET_FRAME_SIZE = 0.02
+SMALER_FRAMES = 10
 
 if __name__ == '__main__':
 
@@ -42,9 +46,19 @@ if __name__ == '__main__':
         "label" : []
     }
 
-    i = 0
-    for index, row in df.iterrows():
-        print(i)
-        i+=1
+    frame_id = 0
+    for index, row in df[:2].iterrows():
+    
+        frame_large = record[row[0] * SAMPLES_PER_FRAME_INPUT : (row[0] + 1) * SAMPLES_PER_FRAME_INPUT]
+        label = row[1]
+
+        for no_of_frame in range(SMALER_FRAMES):
+            
+            frame_small = frame_large[no_of_frame * SAMPLES_PER_FRAME_OUTPUT : (no_of_frame + 1) * SAMPLES_PER_FRAME_OUTPUT]
+            data['id'].append(frame_id)
+            data["samples"].append(frame_small)
+            data['label'].append(label)
+
+            frame_id += 1
 
     pass
