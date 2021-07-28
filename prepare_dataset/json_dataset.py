370526e8751ef9efd871c712aa493a6ba253fb99
Czarek Polak <czarek.pmp@gmail.com>
2021-07-28 13:23:07 +0200

    drop all except voice/unvoiced and ID drop all except voice/unvoiced and ID

--- prepare_dataset/json_dataset.py
+++ prepare_dataset/json_dataset.py
@@ -1,12 +1,19 @@
 from os.path import dirname, abspath, join
 import sys
+import pandas as pd
 
 DIR_PATH = dirname(dirname(abspath(__file__)))
 sys.path.append(DIR_PATH)
 
-DATA_PATH = "wav\long\data_long.wav"
+DATA_PATH = "csv\Frame_descr.csv"
 PATH = join(DIR_PATH,  DATA_PATH)
 
 if __name__ == '__main__':
-    
-    
+
+    # Read row data
+    df = pd.read_csv(PATH)
+
+    # drop all exept voice/unvoiced and ID
+    df = df.drop(columns=[df.columns[-3],df.columns[-2], df.columns[-1]])
+    df = df[df[df.columns[1]].notna()]
+    pass
