10ce9208611b090f6f9d00b26574328934ca977a
Czarek Polak <czarek.pmp@gmail.com>
2021-07-28 14:00:35 +0200

    Added dict for data and iterate by rows

--- prepare_dataset/json_dataset.py
+++ prepare_dataset/json_dataset.py
@@ -1,12 +1,20 @@
 from os.path import dirname, abspath, join
 import sys
 import pandas as pd
+from scipy.io import wavfile
+import json
 
 DIR_PATH = dirname(dirname(abspath(__file__)))
 sys.path.append(DIR_PATH)
 
 DATA_PATH = "csv\Frame_descr.csv"
 PATH = join(DIR_PATH,  DATA_PATH)
+WAV_PATH_REL = "wav\long\data_long.wav"
+WAV_PATH= join(DIR_PATH,  WAV_PATH_REL)
+
+# Frame size in sec
+FRAME_SIZE = 0.2
+TARGET_FRAME_SIZE = 0.02
 
 if __name__ == '__main__':
 
@@ -20,6 +28,23 @@ if __name__ == '__main__':
     df = df.astype(int)
     # remove if not 0 or 1
     df = df[df[df.columns[-1]] < 2]
-    
+
+    # load wav file
+    SR, record = wavfile.read(WAV_PATH)
+
+    # SET number of samples in one frame
+    SAMPLES_PER_FRAME_INPUT = int(FRAME_SIZE * SR)
+    SAMPLES_PER_FRAME_OUTPUT = int(TARGET_FRAME_SIZE * SR)
+
+    data = {
+        "id" : [],
+        "samples" : [],
+        "label" : []
+    }
+
+    i = 0
+    for index, row in df.iterrows():
+        print(i)
+        i+=1
 
     pass
