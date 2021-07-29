16dc0fdcc9fa5ee3120b1e04b2ee7f1490c83da1
Czarek Polak <czarek.pmp@gmail.com>
2021-07-29 19:45:17 +0200

    New file vad.py Test version with 60 frames Succesfully load the data to X and Y

---
+++ vad.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from os.path import dirname, abspath, join
+import numpy as np
+import json
+
+FILE_PATH = "json/dataset_6.json"
+INDEPENDENT_VAR = 'mel'
+DEPENDED_VAR = 'label'
+
+def load_data(file_path):
+
+    with open(file_path, 'r') as f:
+        
+        data = json.load(f)
+        data = json.loads(data)
+
+    X = np.asarray(data['mel'])
+    y = np.asarray(data['label'])
+
+    return X, y
+
+        
+
+
+
+if __name__ == '__main__':
+    
+    X, y = load_data(FILE_PATH)
+
+    pass
